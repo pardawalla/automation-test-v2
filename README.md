f1a33c88819b68ddbe135dc67115453c1c54be81
pardawalla <104052542+pardawalla@users.noreply.github.com>
2023-01-04 08:27:40 +1300

    with working api tests using HTTPClient v5 and updated documentation.

--- README.md
+++ README.md
@@ -1,31 +1,53 @@
 # automation-test-v2
+This project has two main set of tests that are currently working.
+- Web UI tests, found in src/test/java/automation/test/Tests.java. The tests here checkt that from the TradeMe website, `https://api.tmsandbox.co.nz`, you can search for an existing used cars listing, select a listing from the search result and verify some of the data found on the selected listings. 
+- API tests, found in src/test/java/apiTests/test0.java:
+    - Using the TradeMe Sandbox environment retrieve a list of charities, and confirm that `St John` is included in the list.
+    - Send a post request, and verify that it was successful using httpbin.org
 
-Requirements:
-Java JDK: 17 (openjdk 17.0.5 2022-10-18)
-Maven: 3.8.7
+
+To run the tests 
+- Download the appropriate version of the ChromeDriver from [here](https://chromedriver.chromium.org/downloads), and Then update the value for the path in `Tests.java` file with the correct absoulte path. This will be found in the `@BeforeClass`.
+
+The aim here was to write these tests using Java, and Selenium. Preferably in BDD style using [Cucumber] (https://cucumber.io/docs/tools/general/). 
+The (https://testng.org/doc/) test framework was used.
+For the API [HTTPClient v5] (https://hc.apache.org/httpcomponents-client-5.2.x/index.html) was used. 
+
+Initially for API tests tried to use the [Rest-assured] (https://rest-assured.io/) library. However, was unable to use it as kept on getting the error `The import io.restassured.RestAssured cannot be resolved`, and was unable to fix it. After which decided to use the HTTPClient v5 library instead.  
+
+Also was unable to integrate Cucumber, and write the tests in BBD Style.
+
+The code was tested and written in the Following Dev Environment:
+- OS: macOS Ventura 13.1
+- IDE: [Visual Studio Code Version: 1.74.2 (Universal)] (https://code.visualstudio.com/).
+
+Theorectially you should be able to run these tests on Windows/Linux/Mac using VSCode or another IDE. However, using VSCode might be the easiest way to run these tests. 
+
+
+## Requirements:
+- Java JDK: 17 
+    JAVA JDK used:
+    openjdk 17.0.5 2022-10-18
+    OpenJDK Runtime Environment Temurin-17.0.5+8 (build 17.0.5+8)
+    OpenJDK 64-Bit Server VM Temurin-17.0.5+8 (build 17.0.5+8, mixed mode, sharing)
+- Maven: 3.8.7
     https://maven.apache.org/install.html
     https://maven.apache.org/download.cgi
-    Configuringing on env variables on Mac
-        https://stackoverflow.com/questions/22842743/how-to-set-java-home-environment-variable-on-mac-os-x-10-9
-TestNG: 7.7.0
-Selenium: 4.7.2
-Cucumber: https://cucumber.io/docs/tools/general/
+- TestNG: 7.7.0
+- Selenium: 4.7.2
+- Cucumber: https://cucumber.io/docs/tools/general/
+- Visual Studio Extensions 
+  - [Extension Pack for Java] (https://marketplace.visualstudio.com/items?itemName=vscjava.vscode-java-pack)
 
 
-JAVA JDK used:
-openjdk 17.0.5 2022-10-18
-OpenJDK Runtime Environment Temurin-17.0.5+8 (build 17.0.5+8)
-OpenJDK 64-Bit Server VM Temurin-17.0.5+8 (build 17.0.5+8, mixed mode, sharing)
+## References: 
+- https://stackoverflow.com/questions/62163474/how-do-i-setup-vscode-to-create-maven-projects
+- https://www.youtube.com/watch?v=BBy8vLYZbqM
+- [Configuringing env variables on Mac] (https://stackoverflow.com/questions/22842743/how-to-set-java-home-environment-variable-on-mac-os-x-10-9)
 
 
-Useful extensions for Visual Studio Code:
-1. Extension Pack for Java
-References: 
-1. https://stackoverflow.com/questions/62163474/how-do-i-setup-vscode-to-create-maven-projects
-2. https://www.youtube.com/watch?v=BBy8vLYZbqM
-
-
-TODO:
-1. Move test data to a different file.
-2. Figure out why cucumber tests are not running. 
-3. Figure out javadocs and added them for better documentation.
+## TODO:
+- [ ] Figure out why cucumber tests are not running. 
+- [ ] Create Utils, e.g. SeleniumUtils, RESTUtils.
+- [ ] Move test data to a different file.
+- [ ] Figure out javadocs and added them for better documentation.
--- src/test/java/apiTests/test0.java
+++ src/test/java/apiTests/test0.java
@@ -1,19 +1,42 @@
 package apiTests;
 
 import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 
-import org.apache.hc.client5.http.fluent.Request;
-import org.apache.hc.client5.http.fluent.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/**
- * @author https://www.wdbyte.com
- *         Reference:
- *         https://www.springcloud.io/post/2022-08/httpclient5/#gsc.tab=0
- */
+// Required for HTTP requests
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.client5.http.fluent.Response;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.http.message.StatusLine;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+
+import java.util.List;
+import java.util.Map;
+
 public class test0 {
 
+  
+
+    // Get request
+    // Reference:
+    // https://www.springcloud.io/post/2022-08/httpclient5/#gsc.tab=0
     public static String get(String url) {
         String result = null;
         try {
@@ -25,14 +48,73 @@ public class test0 {
         return result;
     }
 
+    // POST request
+    // Reference(s):
+    // https://www.springcloud.io/post/2022-08/httpclient5/#gsc.tab=0
+    // https://jsontostring.com/
+    public static Response post(String url) throws IOException {
+        // String result = null;
+        Response resp = null;
+        Request request = Request.post(url).setHeader("Content-type", "application/json");
+        // JSON String
+        String jsonRequest = "{\"ExistingListingId\":123,\"Category\":\"ABC\",\"Title\":\"ABC\",\"Subtitle\":\"ABC\",\"Description\":[\"ABC\",\"ABC\"],\"StartPrice\":123.0,\"ReservePrice\":123.0,\"BuyNowPrice\":123.0,\"Duration\":0,\"EndDateTime\":\"\\/Date(1514764800)\\/\",\"Pickup\":0,\"PickupSuburbId\":123,\"IsBrandNew\":false,\"AuthenticatedMembersOnly\":false,\"IsClassified\":false,\"OpenHomes\":[{\"Start\":\"\\/Date(1514764800)\\/\",\"End\":\"\\/Date(1514764800)\\/\"},{\"Start\":\"\\/Date(1514764800)\\/\",\"End\":\"\\/Date(1514764800)\\/\"}]}";
+        StringEntity jsonRequestEntity = new StringEntity((jsonRequest));
+        request = request.body(jsonRequestEntity);
+        try {
+            // result = request.execute().returnContent().asString();
+            resp = request.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // return result;
+        return resp;
+    }
+
     String testUrl = "https://api.tmsandbox.co.nz/v1/Charities.json";
+    // At the moment no JSON parser. If the charity is there, it'll have to be
+    // in the format `,"Description":"St John",`. This way we can make sure
+    // the value is from the `Description` key/value pair, and not from the
+    // Tagline, ImageSource etc, key/value pair.
     String expectedCharityDescription = ",\"Description\":\"St John\",";
+
     @Test()
-    public void CharityContainsStJohn() {
-        
+    public void CharityContainsStJohn() throws JsonMappingException, JsonProcessingException {
+
         String respString = get(testUrl);
         System.out.println(respString);
         Assert.assertTrue(respString.contains(expectedCharityDescription));
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        // Reference:
+        // https://stackoverflow.com/questions/36519974/can-not-deserialize-instance-of-java-util-hashmap-out-of-start-array-token
+        List<Map<String, Object>> charities = mapper.readValue(respString,
+                new TypeReference<List<Map<String, Object>>>() {
+                });
+        System.out.println("charities is: " + charities);
+
+        // for (String Description: charities.keySet()) {
+        // String key = Description.toString();
+        // String value = charities.get(Description).toString();
+        // System.out.println(key + "" + value);
+        // }
+    }
+
+    // References:
+    // https://stackoverflow.com/questions/14024625/how-to-get-httpclient-returning-status-code-and-response-body
+    // https://stackoverflow.com/questions/64819417/java-cannot-resolve-method-getentity-in-httpresponse
+    @Test()
+    public void TestPOST() throws IOException, ParseException {
+        Response resp = post("https://httpbin.org/anything");
+        ClassicHttpResponse httpResp = (ClassicHttpResponse) resp.returnResponse();
+        int statusCode = httpResp.getCode();
+        String statusReasonPhrase = httpResp.getReasonPhrase();
+        String responseBody = EntityUtils.toString(httpResp.getEntity(), StandardCharsets.UTF_8);
+
+
+        System.out.println("\nStatu Code:" + statusCode + "\nreason phrase:" + statusReasonPhrase + "\n\n body:" + responseBody);
+        //System.out.println(resp.returnResponse().getCode());
+
     }
 
 }
